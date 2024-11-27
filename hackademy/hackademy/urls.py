a4c2461af843d39ac03566207936f95c63874945
goubexlv <ngoubenejunior@gmail.com>
2024-11-27 18:04:28 +0100

    coming soon hackademy

---
+++ hackademy/hackademy/urls.py
@@ -0,0 +1,12 @@
+
+from django.contrib import admin
+from django.urls import path
+from .views import index
+
+
+urlpatterns = [
+    path('', index, name="index"),
+    
+    path('admin/', admin.site.urls),
+
+]
